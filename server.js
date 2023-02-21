1b46b31d3bc1894a1719ad72bb90f0936c903a48
Ju Dayoung <ekdud7703@naver.com>
2023-02-21 19:56:08 +0900

    feat: express만으로 http request

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ app.get('/', function(req, res){
     res.send(`Hello World!`);
 });
 
+app.post('/', function(req, res){
+    res.send(`Hello World! post.`);
+});
+
+app.put('/', function(req, res){
+    res.send(`Hello World! put.`);
+});
+
+app.delete('/', function(req, res){
+    res.send(`Hello World! delete.`);
+});
+
+
 var server = app.listen(3000, function(){
     var host = server.address().address;
     var port = server.address().port;
