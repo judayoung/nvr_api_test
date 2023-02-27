65ffd97af53f56f35423d4e6b6c97064096551e5
Ju Dayoung <ekdud7703@naver.com>
2023-02-28 08:43:39 +0900

    config: port 지정. 55556

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.delete('/', function(req, res){
 });
 
 
-var server = app.listen(3000, function(){
+var server = app.listen(55556, function(){
     var host = server.address().address;
     var port = server.address().port;
 
