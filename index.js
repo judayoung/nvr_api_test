24b7951dcab64a76371fa09d7ad8b266192cd86d
Ju Dayoung <ekdud7703@naver.com>
2023-03-01 13:32:55 +0900

    feat: simple-server에 requestHandler 추가

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var server = require('./simple-http/server');
 var router = require('./simple-http/router');
+var requestHandlers = require('./simple-http/requestHandlers');
 
-server.start(55559, router.route);
+server.start(55559, router.route, requestHandlers.handle);
--- simple-http/server.js
+++ simple-http/server.js
@@ -3,12 +3,12 @@ exports.start = start;
 var http = require('http');
 var url = require('url');
 
-function start(port, route) {
+function start(port, route, handle) {
     function onRequest(req, res){
         var pathname = url.parse(req.url).pathname;
         console.log(`Request for ${pathname} received.`);
 
-        route(pathname); // injected function call
+        route(handle, pathname); // injected function call
 
         write(res, `pathname=${pathname}`);
     }
