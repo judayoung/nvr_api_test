469ff8e369a68d3a7ae7312efeef0dc6abdec7ff
Ju Dayoung <ekdud7703@naver.com>
2023-03-01 12:37:22 +0900

    feat: simple-server.js 에 이벤트리스너 추가

--- simple-server.js
+++ simple-server.js
@@ -17,3 +17,19 @@ function onRequest(req, res){
 }
 
 http.createServer(onRequest).listen(55557);
+
+var server3 = http.createServer();
+
+server3.addListener('request', function(req, res){
+    console.log(`Server running at http://${req.headers.host}`);
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(`Hello Node. addListener. request.`);
+    res.end();
+});
+
+server3.addListener('connection', function(req, res){
+    console.log('client connected...'); // 왜 두번 찍힐까?
+});
+
+server3.listen(55558);
